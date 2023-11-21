e490e4485c322ea16bf0a29d93e629c2212315ec
ROHIT9991 <142079391+ROHIT9991@users.noreply.github.com>
2023-11-21 23:43:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,51 @@
 document.getElementById('registrationForm').addEventListener('submit', function(event) {
     event.preventDefault();
+
     var name = document.getElementById('name').value;
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
     var dob = document.getElementById('dob').value;
+    var terms = document.getElementById('terms').checked;
+
+    // Validate email
+    var emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    if (!email.match(emailPattern)) {
+        alert('Please enter a valid email address.');
+        return;
+    }
+
+    // Validate age
+    var age = new Date().getFullYear() - new Date(dob).getFullYear();
+    if (age < 18 || age > 55) {
+        alert('Age must be between 18 and 55.');
+        return;
+    }
+
+    // Add entry to table
     var entryList = document.getElementById('entryList');
-    var entry = document.createElement('div');
-    entry.className = 'entries';
-    entry.innerHTML = '<p>' + name + '</p><p>' + email + '</p><p>' + password + '</p><p>' + dob + '</p>';
-    entryList.appendChild(entry);
-    alert('Form submitted successfully!');
+    var row = document.createElement('tr');
+    row.innerHTML = '<td>' + name + '</td><td>' + email + '</td><td>' + password + '</td><td>' + dob + '</td><td>' + (terms ? 'Yes' : 'No') + '</td>';
+    entryList.appendChild(row);
+
+    // Save entries to local storage
+    var entries = JSON.parse(localStorage.getItem('entries')) || [];
+    entries.push({ name: name, email: email, password: password, dob: dob, terms: terms });
+    localStorage.setItem('entries', JSON.stringify(entries));
+});
+
+window.onload = function() {
+    // Clear local storage
+    localStorage.clear();
+
+    var entries = JSON.parse(localStorage.getItem('entries')) || [];
+    entries.forEach(function(entry) {
+        var entryList = document.getElementById('entryList');
+        var row = document.createElement('tr');
+        row.innerHTML = '<td>' + entry.name + '</td><td>' + entry.email + '</td><td>' + entry.password + '</td><td>' + entry.dob + '</td><td>' + (entry.terms ? 'Yes' : 'No') + '</td>';
+        entryList.appendChild(row);
+    });
+};
+
+document.getElementById('registrationForm').addEventListener('submit', function(event) {
+    /* Your existing form submission code here... */
 });
